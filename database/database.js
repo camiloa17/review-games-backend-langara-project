3a6c3ef3302515021a93a6dedfd11796738d7b91
Camilo Dominguez <cdominguez@aluna.co>
2021-03-27 10:29:22 -0700

    Change db connection to a pool

--- database/database.js
+++ database/database.js
@@ -1,12 +1,22 @@
 const mysql = require('mysql2');
 
 
+const connection = mysql.createPool({
+  connectionLimit:10,
+  host: process.env.DBHOST,
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  database: process.env.DBNAME
+});
 
-const connection = mysql.createConnection({
-    host: process.env.DBHOST,
-    user: process.env.DBUSER,
-    password: process.env.DBPASSWORD,
-    database: process.env.DBNAME
-  });
+connection.on('error',(err)=>{
+  console.log(err);
+})
+// const connection = mysql.createConnection({
+//     host: process.env.DBHOST,
+//     user: process.env.DBUSER,
+//     password: process.env.DBPASSWORD,
+//     database: process.env.DBNAME
+//   });
 
 module.exports=connection;
